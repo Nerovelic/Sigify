6bd9177323adfa0d190c1deae6737236bed80390
Nerovelic <zack.vadhir2@hotmail.com>
2024-04-02 10:38:55 -0700

    la lista de documentos se hizo responsivo

--- src/app/components/listDocuments.tsx
+++ src/app/components/listDocuments.tsx
@@ -34,15 +34,17 @@ export default function ListDocuments() {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      {documentRows.map(
-        (row: any[], rowIndex: React.Key | null | undefined) => (
-          <div key={rowIndex} className="flex mb-4">
-            {row.map((_: any, colIndex: React.Key | null | undefined) => (
-              <DocumentItem key={colIndex} />
-            ))}
-          </div>
-        )
-      )}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {documentRows.map(
+          (row: any[], rowIndex: React.Key | null | undefined) => (
+            <React.Fragment key={rowIndex}>
+              {row.map((_: any, colIndex: React.Key | null | undefined) => (
+                <DocumentItem key={colIndex} />
+              ))}
+            </React.Fragment>
+          )
+        )}
+      </div>
     </div>
   );
 }
