4be4c3a3a76cd6ce82fb6037dbb130506b343729
Nerovelic <zack.vadhir2@hotmail.com>
2024-04-03 12:45:00 -0700

    ya se puede arrastrar en el apartado de subir y se cambio el diseño

--- src/app/components/FolderDialog.tsx
+++ src/app/components/FolderDialog.tsx
@@ -7,6 +7,7 @@ import DialogActions from "@material-ui/core/DialogActions";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import ListDocuments from "../listaDocumentos/listDocuments";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 interface FolderDialogProps {
   isOpen: boolean;
@@ -15,13 +16,16 @@ interface FolderDialogProps {
 
 const FolderDialog: React.FC<FolderDialogProps> = ({ isOpen, onClose }) => {
   const [activeTab, setActiveTab] = useState(0);
+  const [isMaximized, setIsMaximized] = useState(false);
 
   const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setActiveTab(newValue);
   };
 
+  const minHeight = isMaximized ? "400px" : "200px";
+
   return (
-    <Dialog open={isOpen} onClose={onClose} fullWidth maxWidth="xl">
+    <Dialog open={isOpen} onClose={onClose} fullWidth maxWidth="sm">
       <DialogTitle>Abrir un archivo</DialogTitle>
       <DialogContent>
         <Tabs
@@ -39,16 +43,46 @@ const FolderDialog: React.FC<FolderDialogProps> = ({ isOpen, onClose }) => {
             <ListDocuments size="w-2/3 h-32" />
           </div>
         )}
-        {activeTab === 1 && (
-          <div style={{ minHeight: "400px", padding: "16px" }}>
-            <p>Contenido de Subir documentos:</p>
+        <div
+          style={{
+            minHeight: minHeight,
+            padding: "16px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }}
+          onDrop={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            const file = e.dataTransfer.files[0];
+            // Aquí puedes manejar el archivo que se soltó
+            console.log("Archivo soltado:", file);
+          }}
+        >
+          <CloudUploadIcon style={{ fontSize: 200, color: "#374347" }} />
+          {activeTab === 1 && (
             <div>
-              <p>Subir un archivo:</p>
-              <input type="file" accept=".docx,.pdf" />
-              <button>Examinar</button>
+              <label
+                htmlFor="fileInput"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-14 rounded inline-block relative cursor-pointer"
+              >
+                Subir archivo
+                <input
+                  id="fileInput"
+                  type="file"
+                  accept=".docx,.pdf"
+                  className="opacity-0 absolute inset-0 w-full h-full cursor-pointer"
+                />
+              </label>
+              <p>o arrastrar aquí un archivo</p>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} color="primary">
--- src/app/components/themeMode.tsx
+++ src/app/components/themeMode.tsx
@@ -1,5 +1,5 @@
 export const toggleTheme = (isDarkMode: boolean) => {
-  const body = document.body; // o selecciona el elemento contenedor
+  const body = document.body;
   if (!isDarkMode) {
     body.classList.remove("dark-mode");
     body.classList.add("light-mode");
